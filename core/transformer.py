389ab2e220b50b216c673cc55b2ba17dc217f456
Deekshith-Dade <deekshithreddy180@gmail.com>
2025-12-29 03:09:21 -0700

    rms norm

--- core/transformer.py
+++ core/transformer.py
@@ -83,6 +83,35 @@ def apply_rotary_emb(
     xk_out = (xk_ * freqs_cis).sum(5).flatten(3)
     return xq_out.astype(xq.dtype), xk_out.astype(xk.dtype)
     
+class RMSNorm(nn.Module):
+    """
+    Initialize the RMSNorm normalization layer.
+
+    Args:
+        dim (int): The dimension of the input tensor.
+        eps (float, optional): A small value added to the denominator for numerical stability. Default is 1e-6.
+
+    Attributes:
+        eps (float): A small value added to the denominator for numerical stability.
+        weight (nn.Parameter): Learnable scaling parameter.
+
+    """
+
+    def __init__(self, dim: int, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(mx.ones((dim, )))
+
+    def _norm(self, x: mx.array):
+        return x * mx.rsqrt((x * x).mean(-1, keepdim=True) + self.eps)
+
+    def forward(self, x: mx.array):
+        # x = probe.log_stats(x, "resid")
+        output = self._norm(x.float())
+        return (output * self.weight.float()).type_as(x)
+
+    def reset_parameters(self):
+        torch.nn.init.ones_(self.weight)  # type: ignore
 
 class Attention(nn.Module):
     def __init__(
